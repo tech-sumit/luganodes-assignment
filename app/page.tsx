dd5b9f4bb9fc76c209bf8b2adca4485a88823b55
Sumit Agrawal <mr.sumitagrawal.17@gmail.com>
2023-12-21 20:00:18 +0530

    Basic cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,38 @@
-import DeployButton from '../components/DeployButton'
 import AuthButton from '../components/AuthButton'
-import { createClient } from '@/utils/supabase/server'
-import ConnectSupabaseSteps from '@/components/ConnectSupabaseSteps'
-import SignUpUserSteps from '@/components/SignUpUserSteps'
-import Header from '@/components/Header'
-import { cookies } from 'next/headers'
+import {cookies} from 'next/headers'
+import Logo from "@/components/Logo";
 
 export default async function Index() {
-  const cookieStore = cookies()
+    const cookieStore = cookies()
 
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
+    return (
+        <div className="flex-1 w-full flex flex-col items-center">
+            <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                <div className="w-full flex justify-between items-center p-3 text-sm">
+                    <Logo/>
+                    <AuthButton/>
+                </div>
+            </nav>
 
-  const isSupabaseConnected = canInitSupabaseClient()
+            <div className="flex-1 flex flex-col opacity-0 px-3">
+                <main className="flex-1 flex flex-col gap-6">
 
-  return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
+                </main>
+            </div>
+
+            <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+                <p>
+                    Powered by{' '}
+                    <a
+                        href="https://sumitagrawal.dev/"
+                        target="_blank"
+                        className="font-bold hover:underline"
+                        rel="noreferrer"
+                    >
+                        Sumit Agrawal
+                    </a>
+                </p>
+            </footer>
         </div>
-      </nav>
-
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
-      </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{' '}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
-    </div>
-  )
+    )
 }
